24371d4b3c4c309eac025229a4f53a0414fb3222
loud <loud@bondoracle.com>
2023-04-09 22:50:04 +0800

    MerkleDistributor

--- contracts/MerkleDistributor.sol
+++ contracts/MerkleDistributor.sol
@@ -1,27 +1,29 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity >=0.8.1 <0.9.0;
-
+pragma solidity >=0.8.0 <0.9.0;
+import {IMerkleDistributor} from "./interfaces/IMerkleDistributor.sol";
+import "./interfaces/AggregatorMerkleInterface.sol";
 import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
-import {IMerkleDistributor} from "./IMerkleDistributor.sol";
 
-import "./interfaces/AggregatorMerkleInterface.sol";
 
 contract MerkleDistributor is IMerkleDistributor {
     using SafeERC20 for IERC20;
 
-    struct ClaimedItem{
+    struct ClaimItem{
+        uint256 _batchId;
         uint256 _index;
         address _account;
         bool _isClaimed;
     }
 
     AggregatorMerkleInterface internal merkleAggregator;
-    address public immutable override token;
-    uint256 public immutable override projectId;
-    bytes32 public immutable override merkleRoot;
-    uint256 public immutable override curBatch;
-    
+    address public   token;
+    uint256 public   projectId;
+    bytes32 public   merkleRoot;
+    uint256 public   curBatch;
+    uint80 public    roundId;
+    uint256 public   startedAt;
+    uint256 public   updatedAt;
 
     // This is a packed array of booleans.
     mapping(uint256 => ClaimItem) private claimedBitMap;
@@ -29,7 +31,7 @@ contract MerkleDistributor is IMerkleDistributor {
     constructor(address aggregatorProxy_, address token_, uint256 projectId_) {
         merkleAggregator = AggregatorMerkleInterface(aggregatorProxy_);
         token = token_;
-        projectId_ = projectId;
+        projectId = projectId_;
     }
 
     function isLocked() public returns (bool){
@@ -40,19 +42,17 @@ contract MerkleDistributor is IMerkleDistributor {
         if(isLocked())
             return false;
         (
-            /*uint80 roundID*/,
-            batch,
+            roundId,
+            curBatch,
             merkleRoot,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-        ) = merkleAggregator.lastMerkleRoot();
+            startedAt,
+            updatedAt
+        ) = merkleAggregator.latestMerkleRoundData();
         return true;
     }
 
     function isClaimed(uint256 batch, uint256 index)
-    public
-    virtual
-    returns (bool) {
+    public view override returns (bool) {
         if(curBatch != batch)
             return true;
         ClaimItem memory claim  =  claimedBitMap[batch];
@@ -61,19 +61,17 @@ contract MerkleDistributor is IMerkleDistributor {
         return false;
     }
 
-    function _setClaimed(uint256 index) private {
+    function _setClaimed(uint256 index, address account) private {
         claimedBitMap[index] = ClaimItem({
-        _batch:curBatch,
+        _batchId:curBatch,
         _index:index,
+        _account:account,
         _isClaimed:true
         });
     }
 
     function claim(uint256 batch, uint256 index, address account, uint256 amount, bytes32[] calldata merkleProof)
-    public
-    virtual
-    returns (bool)
-    {
+    public  virtual override returns (bool) {
         if(isLocked()) return false;
         if(batch != curBatch) return false;
         if (isClaimed(curBatch,index)) return false;
@@ -85,9 +83,7 @@ contract MerkleDistributor is IMerkleDistributor {
         // Mark it claimed and send the token.
         _setClaimed(index, account);
 
-
         IERC20(token).safeTransfer(account, amount);
-
         emit Claimed(batch, index, account, amount);
         return true;
     }
---
+++ scripts/deploy.ts
@@ -0,0 +1,24 @@
+import { ethers } from "hardhat";
+
+async function main() {
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  const unlockTime = currentTimestampInSeconds + 60;
+
+  const lockedAmount = ethers.utils.parseEther("0.001");
+
+  const Lock = await ethers.getContractFactory("MerkleDistributor");
+  const lock = await Lock.deploy("0xfd5a6B3206AEd3232F3e3f836197A350A874404a", "0xfd5a6B3206AEd3232F3e3f836197A350A874404a", 12);
+
+  await lock.deployed();
+
+  console.log(
+    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
